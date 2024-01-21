9a3cdd4bbaf40822fe7ada6b85c259e382b00560
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-21 13:27:56 +0000

    chore: Changed the name of API gateway.

--- infrastructure/aws/modules/api_gateway/main.tf
+++ infrastructure/aws/modules/api_gateway/main.tf
@@ -2,31 +2,31 @@ provider "aws" {
   region     = var.AWS_REGION
 }
 
-resource "aws_api_gateway_rest_api" "TaskTraceZAPIGateway" {
-  name        = "tasktracez-apigateway"
+resource "aws_api_gateway_rest_api" "TaskTraceZAPIGatewayRestApiRestApi" {
+  name        = "tasktracez-apigateway-rest-api"
   description = "This is my API for demonstration purposes"
 }
 
 resource "aws_api_gateway_resource" "TaskTraceZPrefixResource" {
-  parent_id   = aws_api_gateway_rest_api.TaskTraceZAPIGateway.root_resource_id
+  parent_id   = aws_api_gateway_rest_api.TaskTraceZAPIGatewayRestApi.root_resource_id
   path_part   = "api"
-  rest_api_id = aws_api_gateway_rest_api.TaskTraceZAPIGateway.id
+  rest_api_id = aws_api_gateway_rest_api.TaskTraceZAPIGatewayRestApi.id
 }
 
 resource "aws_api_gateway_resource" "TaskTraceZUserResource" {
   parent_id   = aws_api_gateway_resource.TaskTraceZPrefixResource.id
   path_part   = "user"
-  rest_api_id = aws_api_gateway_rest_api.TaskTraceZAPIGateway.id
+  rest_api_id = aws_api_gateway_rest_api.TaskTraceZAPIGatewayRestApi.id
 }
 
 resource "aws_api_gateway_resource" "TaskTraceZProjectsResource" {
   parent_id   = aws_api_gateway_resource.TaskTraceZUserResource.id
   path_part   = "projects"
-  rest_api_id = aws_api_gateway_rest_api.TaskTraceZAPIGateway.id
+  rest_api_id = aws_api_gateway_rest_api.TaskTraceZAPIGatewayRestApi.id
 }
 
 resource "aws_api_gateway_method" "TaskTraceZGetUserProjectsMethod" {
-  rest_api_id   = aws_api_gateway_rest_api.TaskTraceZAPIGateway.id
+  rest_api_id   = aws_api_gateway_rest_api.TaskTraceZAPIGatewayRestApi.id
   resource_id   = aws_api_gateway_resource.TaskTraceZProjectsResource.id
   http_method   = "GET"
   authorization = "NONE"
