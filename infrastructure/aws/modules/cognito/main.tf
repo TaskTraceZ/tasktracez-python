437b91f1c0b3921d57cc156e5b3a9d56c501fc63
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-21 14:42:27 +0000

    feat: Added email recovery options in cognito.

--- infrastructure/aws/modules/cognito/main.tf
+++ infrastructure/aws/modules/cognito/main.tf
@@ -3,5 +3,12 @@ provider "aws" {
 }
 
 resource "aws_cognito_user_pool" "TaskTraceZCognitoUserPool" {
-  name = "tasktracez-cognito-user-pool"
+  name             = "tasktracez-cognito-user-pool"
+  alias_attributes = ["preferred_username"]
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
 }
