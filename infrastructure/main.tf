defec8c81febd3b3d2801a53f4bc22f9d8a7e182
SUBHAYAN DASGUPTA <52292499+subhayanDsgpt-GitHub@users.noreply.github.com>
2024-01-22 00:03:39 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,18 +9,7 @@ terraform {
 
 provider "aws" {}
 
-# module "aws_ecr" {
-#   source = "./aws/modules/ecr"
-# }
-
-# module "aws_cognito" {
-#   source = "./aws/modules/cognito"
-# }
-
-# module "aws_apigateway" {
-#   source = "./aws/modules/api_gateway"
-# }
-
+# Creating a test VPC with Terraform Cloud
 resource "aws_vpc" "main" {
   cidr_block       = "10.123.0.0/16"
   instance_tenancy = "default"
