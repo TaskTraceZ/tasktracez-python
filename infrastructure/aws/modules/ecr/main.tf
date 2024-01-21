a2d6e8fe7f783ef58b18b274d2302064cb10391e
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-21 11:21:00 +0000

    feat: Removed the functionality to build the lambda from Terraform.

--- infrastructure/aws/modules/ecr/main.tf
+++ infrastructure/aws/modules/ecr/main.tf
@@ -6,17 +6,3 @@ resource "aws_ecr_repository" "TaskTraceZUserProjectsGetterRepository" {
   name                 = "tasktracez-user-projects-getter-repository"
   image_tag_mutability = "MUTABLE"
 }
-
-resource "null_resource" "BuildAndPushTaskTraceZUserProjectsGetterImage" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      docker build -t ${aws_ecr_repository.TaskTraceZUserProjectsGetterRepository.repository_url}:latest .
-      aws ecr get-login-password --region ${var.AWS_REGION} | docker login --username AWS --password-stdin ${aws_ecr_repository.TaskTraceZUserProjectsGetterRepository.repository_url}
-      docker push ${aws_ecr_repository.TaskTraceZUserProjectsGetterRepository.repository_url}:latest
-    EOT
-  }
-}
