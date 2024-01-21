d1447888513edf285a40a45500495a6c4c7301a1
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-21 13:21:11 +0000

    feat: included cognito module in main.tf.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,10 +7,14 @@ terraform {
   }
 }
 
-module "aws_apigateway" {
-    source = "./aws/modules/api_gateway"
+module "aws_ecr" {
+  source = "./aws/modules/ecr"
 }
 
-module "aws_ecr" {
-    source = "./aws/modules/ecr"
+module "aws_cognito" {
+  source = "./aws/modules/cognito"
+}
+
+module "aws_apigateway" {
+  source = "./aws/modules/api_gateway"
 }
