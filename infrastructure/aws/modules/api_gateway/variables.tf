c897b50a0b1dc3b846dac518f003f7700b710e8d
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-21 11:04:34 +0000

    feat: Experimenting with variables

--- infrastructure/aws/modules/api_gateway/variables.tf
+++ infrastructure/aws/modules/api_gateway/variables.tf
@@ -1,14 +1,4 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "value"
-}
-
-variable "AWS_SECRET_KEY" {
-    type = string
-    default = "value"
-}
-
 variable "AWS_REGION" {
     type = string
-    default = "value"
+    default = ""
 }
