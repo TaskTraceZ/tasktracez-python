2977f71864bffbec304cd3bbc0de4b10d2a86675
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-22 00:56:11 +0000

    fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,25 +7,14 @@ terraform {
   }
 }
 
-provider "aws" {}
+module "aws_ecr" {
+  source = "./aws/modules/ecr"
+}
 
-# module "aws_ecr" {
-#   source = "./aws/modules/ecr"
-# }
+module "aws_cognito" {
+  source = "./aws/modules/cognito"
+}
 
-# module "aws_cognito" {
-#   source = "./aws/modules/cognito"
-# }
-
-# module "aws_apigateway" {
-#   source = "./aws/modules/api_gateway"
-# }
-
-resource "aws_vpc" "main" {
-  cidr_block       = "10.123.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "test_vpc"
-  }
+module "aws_apigateway" {
+  source = "./aws/modules/api_gateway"
 }
