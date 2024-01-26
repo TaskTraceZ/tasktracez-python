cbb5fce0c02430fbec6547cd87636007618560e1
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-26 14:02:54 +0000

    feat: Configured task deleter lambda.

--- src/task/deleter/main.py
+++ src/task/deleter/main.py
@@ -1,4 +1,3 @@
-import json
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
@@ -9,7 +8,7 @@ load_dotenv()
 
 def handler(event, context):
     user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
-    project_id = event["pathParameters"]["project"]
+    task_id = event["pathParameters"]["task"]
 
     sqlalchemy_engine = create_engine(
         f"postgresql+pg8000://{os.environ['DATABASE_USERNAME']}:{os.environ['DATABASE_PASSWORD']}@{os.environ['DATABASE_URL']}/{os.environ['DATABASE_NAME']}",
@@ -17,19 +16,17 @@ def handler(event, context):
     )
 
     with sqlalchemy_engine.connect() as sqlalchemy_engine_connection:
-        project_deleter_query = text(
+        task_deleter_query = text(
             """
                 DELETE
                 FROM
-                    projects
+                    tasks
                 WHERE
-                    id = :project_id
+                    id = :task_id
             """
         )
 
-        sqlalchemy_engine_connection.execute(
-            project_deleter_query, {"project_id": project_id}
-        )
+        sqlalchemy_engine_connection.execute(task_deleter_query, {"task_id": task_id})
 
         sqlalchemy_engine_connection.commit()
 
