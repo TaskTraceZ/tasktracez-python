044c62ef06d8c301673e5058e091d9d416199eff
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-01-26 13:29:33 +0000

    feat: Updated lambda function to update a task, to support specifying properties in the updation query dynamically.

--- src/task/updater/main.py
+++ src/task/updater/main.py
@@ -9,12 +9,21 @@ load_dotenv()
 
 def handler(event, context):
     user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
-    project_id = event["pathParameters"]["project"]
+    task_id = event["pathParameters"]["task"]
 
     body = json.loads(event["body"])
 
-    title = body["title"]
-    description = body["description"] if "description" in body else ""
+    set_clauses_list = []
+    parameters = {}
+
+    for key in body:
+        set_clauses_list.append(f"{key} = :{key}")
+
+        parameters[key] = body[key]
+
+    parameters["task_id"] = task_id
+
+    set_clauses = ",\n\t\t\t".join(set_clauses_list)
 
     sqlalchemy_engine = create_engine(
         f"postgresql+pg8000://{os.environ['DATABASE_USERNAME']}:{os.environ['DATABASE_PASSWORD']}@{os.environ['DATABASE_URL']}/{os.environ['DATABASE_NAME']}",
@@ -22,27 +31,23 @@ def handler(event, context):
     )
 
     with sqlalchemy_engine.connect() as sqlalchemy_engine_connection:
-        project_updater_query = text(
-            """
+        task_updater_query = text(
+            f"""
                 UPDATE
-                    projects
+                    tasks
                 SET
-                    title = :title,
-                    description = :description
+                    {set_clauses}
 
                 WHERE
-                    id = :project_id
+                    id = :task_id;
             """
         )
 
-        sqlalchemy_engine_connection.execute(
-            project_updater_query,
-            {"title": title, "description": description, "project_id": project_id},
-        )
+        sqlalchemy_engine_connection.execute(task_updater_query, parameters)
 
         sqlalchemy_engine_connection.commit()
 
-        project_fetcher_query = text(
+        task_fetcher_query = text(
             """
                 SELECT
                     id,
@@ -51,16 +56,16 @@ def handler(event, context):
                     to_char(created_at, 'YYYY-MM-DD HH24:MI:SS') AS created_at,
                     to_char(updated_at, 'YYYY-MM-DD HH24:MI:SS') AS updated_at
                 FROM
-                    projects
+                    tasks
                 WHERE
-                    id = :project_id
+                    id = :task_id
             """
         )
 
-        project_result = sqlalchemy_engine_connection.execute(
-            project_fetcher_query, {"project_id": project_id}
+        task_result = sqlalchemy_engine_connection.execute(
+            task_fetcher_query, {"task_id": task_id}
         )
 
-        project_result_dict = dict(project_result.mappings().fetchone())
+        task_result_dict = dict(task_result.mappings().fetchone())
 
-        return {"statusCode": 200, "body": json.dumps(project_result_dict)}
+        return {"statusCode": 200, "body": json.dumps(task_result_dict)}
